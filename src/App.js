5a1a3654e91ec927ca1ad3213943720ed70c1fad
EunseoKim <dmstj9408@gmail.com>
2024-08-31 19:27:20 +0900

    🎉 first commit :route 구성하기

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Hompage/Homepage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+// 총 페이지 개수 : 3개
+// 1. 홈페이지 "/" 
+// 2. 영화 전체 보여주는 페이지 + 서치 "/movies"
+// 3. 영화 상세 페이지 "/movies/:id"
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<AppLayout/>}>
+        <Route index element={<Homepage/>}/>
+        <Route path="movies">
+          <Route index element={<MoviePage/>}/>
+          <Route path=":id" element={<MovieDetailPage/>}/>
+        </Route>
+      </Route>
+      
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
+  );
+}
+
+export default App;
