7ea6b517ccb3ca770b91238867bd17e020f4db92
EunseoKim <dmstj9408@gmail.com>
2024-09-03 21:44:45 +0900

    ♻️ MovieSlider와 MovieCard Common 폴더로 옮기기

---
+++ src/common/MovieSlider/MovieSlider.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './MovieSlider.style.css'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import MovieCard from '../MovieCard/MovieCard';
+export const MovieSlider = ({ title, movies,responsive }) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+        <Carousel
+          infinite={true}
+          centerMode={true}
+          itemClass="movie-slider p-1"
+          containerClass="carousel-container"
+          responsive={responsive}
+        >
+          {movies?.map((movie,index) => (
+            <MovieCard movie={movie} key={index}/>
+          ))}
+        </Carousel>;
+    </div>
+  )
+}
