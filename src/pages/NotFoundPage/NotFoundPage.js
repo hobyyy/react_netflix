18321661eb5d51e2d64ecf91b0f94013cd649032
EunseoKim <dmstj9408@gmail.com>
2024-09-04 23:30:16 +0900

    ✅ NotFoundPage : 검색결과 없을 때는 다른 멘트설정

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './NotFoundPage.style.css'
 
-const NotFoundPage = () => {
+const NotFoundPage = ({errorType}) => {
+
+  console.log('err',errorType);
+  
+
   return (
-    <div>NotFoundPage</div>
+   <div class="container">
+    <div class="copy-container center-xy">
+      <p>
+        {errorType=='noSearch' ? 'Search results not found' : '404, page not found.'}
+      </p>
+      <span class="handle"></span>
+  
+    </div>
+  </div>
+ 
   )
 }
 
