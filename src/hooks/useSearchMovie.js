29232d1855d64061854540023bf6d7675d11f070
EunseoKim <dmstj9408@gmail.com>
2024-09-04 23:29:23 +0900

    ✅ genre toggle 클릭기능

--- src/hooks/useSearchMovie.js
+++ src/hooks/useSearchMovie.js
@@ -1,15 +1,19 @@
 import { useQuery } from "@tanstack/react-query"
 import api from "../utils/api"
 
-const fetchSearchMovie = ({keyword, page}) => {
-  return keyword
-    ? api.get(`/search/movie?query=${keyword}&page=${page}`) 
-    : api.get(`/movie/popular?page=${page}`)
+const fetchSearchMovie = ({keyword, genre, page}) => {
+  
+  if(keyword) {
+    return api.get(`/search/movie?query=${keyword}&page=${page}`) 
+  }else if(genre) {
+    return api.get(`/discover/movie?with_genres=${genre}&page=${page}`)
+  }
+  return api.get(`/movie/popular?page=${page}`)
 }
-export const useSearchMovieQuery = ({keyword, page}) => {
+export const useSearchMovieQuery = ({keyword, genre, page}) => {
   return useQuery({
-    queryKey:['movie-search',{keyword, page}],
-    queryFn:()=>fetchSearchMovie({keyword, page}),
+    queryKey:['movie-search',{keyword,genre, page}],
+    queryFn:()=>fetchSearchMovie({keyword, genre, page}),
     select:(result) => result.data 
   })   
 }
--- src/pages/Movies/MoviePage.js
+++ src/pages/Movies/MoviePage.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchMovieQuery } from '../../hooks/useSearchMovie'
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { Alert, Container, Row, Col, Spinner } from 'react-bootstrap';
 import MovieCard from '../../common/MovieCard/MovieCard.js'
 import ReactPaginate from 'react-paginate';
-
+import { useMovieGenreQuery } from '../../hooks/useMovieGenre.js';
+import './MoviePage.style.css';
+import NotFoundPage from '../NotFoundPage/NotFoundPage.js';
 // 경로 2가지
 // 1. nav-bar에서 클릭해서 온 경우 => popularMovie 보여주기
 // 2. keyword를 입력해서 온 경우 => keyword와 관련된 영화들을 보여주기
@@ -20,13 +22,28 @@ const MoviePage = () => {
   
   // page state
   const [page,setPage] = useState(1);
-
-  const {data,isLoading,isError,error} = useSearchMovieQuery({keyword, page});
-  // console.log('ddd',data)
-
   const handlePageClick = ({selected}) => {
     setPage(selected + 1);
   }
+  
+  // genre toggle
+  const [selectedMovies,setSelectedMovies] = useState([]);
+  const genre = query.get("g");
+
+  const {data:genreData} = useMovieGenreQuery();
+  const {data:movieData,isLoading,isError,error} = useSearchMovieQuery({keyword, genre, page});
+  const navigate = useNavigate();
+  // console.log('ddd',data)
+
+  useEffect(() => {
+    if(!keyword && genre) {
+      const filteredMovieArray = movieData?.results?.filter((movie) => 
+        genre ? movie.genre_ids.includes(Number(genre)) : true
+      ) || [];
+      setSelectedMovies(filteredMovieArray);
+    }
+  }, [movieData, genre, page, keyword])
+
   if(isLoading) {
     return (
       <div className='spinner-area'>
@@ -41,42 +58,74 @@ const MoviePage = () => {
   if(isError) {
     return <Alert variant='danger'>{error.message}</Alert>
   }
+
+  // const showMoviesByGenre = (genreID) => {
+  //   navigate(`/movies?g=${genreID}`)
+  //   const filteredMovieArray = movieData?.results?.filter((movie) => 
+  //     genreID ? movie.genre_ids.includes(Number(genreID)) : true
+  //   ) || [];
+  //   console.log('filteredMovieArray', filteredMovieArray)
+  //   setPage(1);
+  //   return setSelectedMovies(filteredMovieArray);
+  // }
+  
+  const showMoviesByGenre = (genreId) => {
+    navigate(`/movies?g=${genreId}`)
+    const filteredMovieArray = movieData?.results?.filter((movie) =>
+      genreId ? movie.genre_ids.includes(Number(genreId)) : true
+    ) || [];
+    console.log('filteredMovieArray', filteredMovieArray)
+    // setPage(1);
+    setSelectedMovies(filteredMovieArray);
+  }
+  console.log('data1',movieData)
+  console.log('data2',genreData)
   return (
-    <Container>
-      <Row>
-        <Col lg={4} xs={12}>필터</Col>
-        <Col lg={8} xs={12}>
+    <div>
+      {movieData?.total_results === 0 
+        ? <NotFoundPage errorType={'noSearch'}></NotFoundPage>
+        : <Container>
+          <div className='moviepage-genreList'>
+            {genreData?.map((genre,index) => (
+              <button key={index} onClick={()=>showMoviesByGenre(genre.id)}>{genre.name}</button>
+            ))}
+          </div>
           <Row>
-            {data?.results.map((movie,index)=>
-              <Col key={index}>
-                <MovieCard movie={movie}/>
-              </Col>
-            )}
+            <Col>
+              <Row>
+                {movieData?.results.map((movie,index)=>
+                  <Col key={index}>
+                    <MovieCard movie={movie}/>
+                  </Col>
+                )}
+              </Row>
+                <ReactPaginate
+                  nextLabel="next >"
+                  onPageChange={handlePageClick}
+                  pageRangeDisplayed={3}
+                  marginPagesDisplayed={2}
+                  pageCount={movieData?.total_pages} // 전체 page
+                  previousLabel="< previous"
+                  pageClassName="page-item"
+                  pageLinkClassName="page-link"
+                  previousClassName="page-item"
+                  previousLinkClassName="page-link"
+                  nextClassName="page-item"
+                  nextLinkClassName="page-link"
+                  breakLabel="..."
+                  breakClassName="page-item"
+                  breakLinkClassName="page-link"
+                  containerClassName="pagination"
+                  activeClassName="active"
+                  renderOnZeroPageCount={null}
+                  forcePage={page-1}
+                />
+            </Col>
           </Row>
-            <ReactPaginate
-              nextLabel="next >"
-              onPageChange={handlePageClick}
-              pageRangeDisplayed={3}
-              marginPagesDisplayed={2}
-              pageCount={data?.total_pages} // 전체 page
-              previousLabel="< previous"
-              pageClassName="page-item"
-              pageLinkClassName="page-link"
-              previousClassName="page-item"
-              previousLinkClassName="page-link"
-              nextClassName="page-item"
-              nextLinkClassName="page-link"
-              breakLabel="..."
-              breakClassName="page-item"
-              breakLinkClassName="page-link"
-              containerClassName="pagination"
-              activeClassName="active"
-              renderOnZeroPageCount={null}
-              forcePage={page-1}
-            />
-        </Col>
-      </Row>
-    </Container>
+        </Container>
+      }
+    </div>
+    
   )
   
 }
